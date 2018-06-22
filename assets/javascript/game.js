37c00ddf40f87b73813be8905275de27f0fae460
Eric Eissler <ericeissler@Erics-MacBook-Pro.local>
2018-06-21 22:39:35 -0500

    html and js

---
+++ assets/javascript/game.js
@@ -0,0 +1,100 @@
+//-------Variables------------//
+var wins = 0;
+var losses = 0;
+var computerNumber = (Math.floor(Math.random * 101) +19;
+$("#computernumber").text(computerNumber);
+$("#wins").html(wins);
+$("#losses").html(loses);
+console.log(computerNumber);
+var score = 0;
+$("#score").text(score);
+
+var cry1 = (Math.floor(Math.random * 111) +1);
+var cry2 = (Math.floor(Math.random * 111) +1);
+var cry3 = (Math.floor(Math.random * 111) +1);
+var cry4 = (Math.floor(Math.random * 111) +1);
+
+//-------Functions----------//
+
+function restart() {
+
+    ComputerNumber = Math.floor(Math.random() * 101) + 19;
+    $("#computernumber").text(computerNumber);
+    Cry1 = Math.floor(Math.random() * 11) + 1;
+    Cry2 = Math.floor(Math.random() * 11) + 1;
+    Cry3 = Math.floor(Math.random() * 11) + 1;
+    Cry4 = Math.floor(Math.random() * 11) + 1;
+    score = 0;
+    $("#score").text(score);
+    
+    }
+
+function win() {
+    alert ("You Win!!!");
+    wins ++;
+    $("#wins").text(wins);
+    restart();
+
+}    
+
+function lose() {
+    alert ("You Lose!!!");
+    losses --;
+    $("#losses").text(loses);
+    restart();
+
+}
+    
+// ----------Events------------//
+
+$('#cry1').on ('click', function(){
+    score = score + cry1;
+    
+    $('#curScore').text(score); 
+          
+        if (score == computerNumber){
+          win();
+        }
+        else if (score > computerNumber){
+          lose();
+        }   
+  });  
+
+$('#cry2').on ('click', function(){
+    score = score + cry2;
+    
+    $('#curScore').text(score); 
+         
+        if (score == computerNumber){
+          win();
+        }
+        else if (score > computerNumber){
+          lose();
+        }   
+  });  
+
+$('#cry3').on ('click', function(){
+    score = score + cry3;
+    
+    $('#curScore').text(score); 
+          
+        if (score == computerNumber){
+          win();
+        }
+        else if (score > computerNumber){
+          lose();
+        }   
+  });  
+
+$('#cry4').on ('click', function(){
+    score = score + cry4;
+    
+    $('#curScore').text(score); 
+         
+        if (score == computerNumber){
+          win();
+        }
+        else if (score > computerNumber){
+          lose();
+        }   
+  });  
