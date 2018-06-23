b77f9431b76b9968eb3e3579fadad30f68a81c3a
Eric Eissler <ericeissler@Erics-MacBook-Pro.local>
2018-06-23 17:37:36 -0500

    fixed js bugs

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,127 +1,107 @@
+
 //-------Variables------------//
-var wins = 0;
-var losses = 0;
-var computerNumber = (Math.floor(Math.random * (120 - 19)) + 19);
-// var myCrystals = [src="../images/red_cry.jpg"]
-$("#computernumber").text(computerNumber);
-$("#wins").text(wins);
-$("#losses").text(losses);
-console.log(computerNumber);
-var score = 0;
-$("#score").text("Score: " + score);
-var cry1 = (Math.floor(Math.random * (1 - 12)) + 1);
-var cry2 = (Math.floor(Math.random * (1 - 12)) + 1);
-var cry3 = (Math.floor(Math.random * (1 - 12)) + 1);
-var cry4 = (Math.floor(Math.random * (1 - 12)) + 1);
+window.wins = 0;
+window.losses = 0;
+window.computerNumber = 0;
+window.score = 0;
+window.cry1 = 0;
+window.cry2 = 0;
+window.cry3 = 0;
+window.cry4 = 0;
 
-console.log(cry1);
-console.log(cry2);
-console.log(cry3);
-console.log(cry4);
-
-
-// start game
-generateVariables = false;
-if (generateVariables) {
-  break;
-}
-else {
-  generateVariables = true;
-  restart();
-}
-//-------Functions----------//
-
-// function createCrystals() {
-//   for (var i = 0; i < 4; i++) {
-//     $("#crystals").append(
-//       `<img class="crystal" src=${myCrystals[i]}>`
-//     )
-//   }
-// }
-
-function restart() {
-
-  ComputerNumber = Math.floor(Math.random() * (120 - 19)) + 19;
-  $("#computernumber").text("Number to reach: " + computerNumber);
-  Cry1 = Math.floor(Math.random() * (1 - 12)) + 1;
-  Cry2 = Math.floor(Math.random() * (1 - 12)) + 1;
-  Cry3 = Math.floor(Math.random() * (1 - 12)) + 1;
-  Cry4 = Math.floor(Math.random() * (1 - 12)) + 1;
-  score = 0;
-  $("#score").text("Score: " + score);
-  generateVariables = false;
-  createCrystals();
-
-}
-
-function win() {
-  alert("You Win!!!");
-  wins++;
-  $("#wins").text("Wins: " + wins);
+$(document).ready(function (e) {
+  
   restart();
 
-}
+  function restart() {
 
-function lose() {
-  alert("You Lose!!!");
-  losses--;
-  $("#losses").text("Losses: " + losses);
-  restart();
-
-}
-
-// ----------Events------------//
+    window.computerNumber = getRandomNumber(19,120);
+    window.cry1 = getRandomNumber(1,12);
+    window.cry2 = getRandomNumber(1,12);
+    window.cry3 = getRandomNumber(1,12);
+    window.cry4 = getRandomNumber(1,12);
+    window.score = 0;
 
 
+    console.log(window.cry1);
+    console.log(window.cry2);
+    console.log(window.cry3);
+    console.log(window.cry4);
+    console.log(window.computerNumber);
 
-$('#cry1').on('click', function () {
-  score = score + cry1;
+    updateHtml();
 
-  $('score').text("Score: " + score);
-
-  if (score == computerNumber) {
-    win();
   }
-  else if (score > computerNumber) {
-    lose();
+
+  function win() {
+    alert("You Win!!!");
+    window.wins++;
+    $("#wins").text("Wins: " + wins);
+    restart();
+
   }
+
+  function lose() {
+    alert("You Lose!!!");
+    window.losses++;
+    $("#losses").text("Losses: " + losses);
+    restart();
+
+  }
+
+  function getRandomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+  }
+
+  function updateHtml () {
+    // var myCrystals = [src="../images/red_cry.jpg"]
+    $("#computernumber").text("Number to reach: " + window.computerNumber);
+    $("#wins").text("Wins: " + window.wins);
+    $("#losses").text("Losses: " + window.losses);
+    $("#score").text("Score: " + window.score);
+  }
+
+  // ----------Events------------//
+
+
+
+  $('#cry1, #cry2, #cry3, #cry4').on('click', function () {
+
+    var thisID = $(this).attr("id");
+    var point = 0;
+
+    
+
+    switch(thisID) {
+      case "cry1":
+        point = window.cry1;
+        break;
+      case "cry2":
+        point = window.cry2;
+        break;
+      case "cry3":
+        point = window.cry3;
+        break;
+      case "cry4":
+        point = window.cry4;
+        break;
+      default:
+        point = 0;
+    }
+
+    window.score += point;
+    $(this).html(point);
+
+    $('score').text("Score: " + window.score);
+
+    updateHtml();
+
+    if (window.score == window.computerNumber) {
+      win();
+    }
+    else if (window.score > window.computerNumber) {
+      lose();
+    }
+  });
+
 });
-
-$('#cry2').on('click', function () {
-  score = score + cry2;
-
-  $('#score').text("Score: " + score);
-
-  if (score == computerNumber) {
-    win();
-  }
-  else if (score > computerNumber) {
-    lose();
-  }
-});
-
-$('#cry3').on('click', function () {
-  score = score + cry3;
-
-  $('#score').text("Score: " + score);
-
-  if (score == computerNumber) {
-    win();
-  }
-  else if (score > computerNumber) {
-    lose();
-  }
-});
-
-$('#cry4').on('click', function () {
-  score = score + cry4;
-
-  $('#score').text("Score: " + score);
-
-  if (score == computerNumber) {
-    win();
-  }
-  else if (score > computerNumber) {
-    lose();
-  }
-});  
